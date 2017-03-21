df6b48fff28fd2cf1958b9116366d6d0d09d9e58
Tatiana <tsg1204@gmail.com>
2017-03-21 16:00:50 -0400

    dist error log etc added

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -1,22 +1,17 @@
 import express from'express';
 import path from 'path';
 import open from 'open';
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
-
+import compression from 'compression';
 /* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
-const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
+app.use(compression());
+app.use(express.static('dist'));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.get('/users', function(req, res) {
@@ -33,6 +28,6 @@ app.listen(port, function(err) {
   if (err) {
     console.log(err);
   } else {
-    open('http://localhost:' + port);
+    open(`http://localhost:${port}`);
   }
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
 import './index.css';
+import {getUsers, deleteUser} from './api/userApi';
 
-import numeral from 'numeral';
+// import numeral from 'numeral';
 
-const courseValue = numeral(1000).format('$0,0.00');
-//debugger;
-/* eslint-disable no-console */
-console.log(`I would pay ${courseValue} for this awesome course!`);
+// const courseValue = numeral(1000).format('$0,0.00');
+// //debugger;
+// /* eslint-disable no-console */
+// console.log(`I would pay ${courseValue} for this awesome course!`);
+
+// Populate table of users via API call
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
+  global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must use array.from to create a real array from a DOM collection
+  // getElementsByClassname only returns an "array like" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+
+});
+
